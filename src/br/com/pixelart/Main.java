0073a29534700e76ec5cfdabfcb3790b97c9bf53
Damares Gaia <damaresgaia@gmail.com>
2025-07-27 12:18:58 -0300

    feat: criar janela principal com painel de pixels e seletor de cor

--- src/br/com/pixelart/Main.java
+++ src/br/com/pixelart/Main.java
@@ -1,15 +1,29 @@
 package br.com.pixelart;
 
+import br.com.pixelart.ui.PixelGridPanel;
+
 import javax.swing.*;
-import br.com.pixelart.ui.PixelBoardPanel;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Pixel Art Game");
-            PixelBoardPanel board = new PixelBoardPanel();
+            PixelGridPanel gridPanel = new PixelGridPanel(16, 16);
 
-            frame.add(board);
+            JPanel controlPanel = new JPanel();
+            JButton colorButton = new JButton("Escolher cor");
+            colorButton.addActionListener(e -> {
+                Color selected = JColorChooser.showDialog(frame, "Escolha uma cor", Color.BLACK);
+                if (selected != null) {
+                    gridPanel.setSelectedColor(selected);
+                }
+            });
+            controlPanel.add(colorButton);
+
+            frame.setLayout(new BorderLayout());
+            frame.add(gridPanel, BorderLayout.CENTER);
+            frame.add(controlPanel, BorderLayout.SOUTH);
             frame.pack();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
